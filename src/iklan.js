9a1055954ec11a7eebbe5077f83b4bb7e8d42c58
lilzinn <80847791+lilzinn@users.noreply.github.com>
2021-03-19 18:10:32 -0300

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ LISTA DE ALUGUEL E CRIAR BOTS:
+╠➥ ALUGUEL: 10 / GRUPO (MÊS)
+╠➥ CRIAR: 35 (PODE SER PROPRIETÁRIO)
+╠➥ PODE PAGAR ATRAVÉS DE:
+╠➥ MERCADO PAGO, BOLETO,ETC...
+╠═══════════════════════════
+╠➥ VANTAGENS
+╠➥ EDIT: ISSO ESTAR DESATIVADO!
+║
+╚═〘  MIDORIYA BOT  〙
+`
+}
+exports.iklan = iklan
